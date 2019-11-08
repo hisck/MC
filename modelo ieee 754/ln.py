39ab42275c57ebe388c96c29e345837da035eaa7
unknown <ra99829@uem.br>
2019-11-07 22:23:18 -0300

    talvez

--- modelo ieee 754/ln.py
+++ modelo ieee 754/ln.py
@@ -3,7 +3,12 @@ from math import factorial
 from math import pow
 
 def calculate_ln(x):
-    return ((x-1) - (pow(x-1, 2)/2) + (pow(x-1, 3)/3) - (pow(x-1, 4)/4) + (pow(x-1, 5)/5) - (pow(x-1, 6)/6) + (pow(x-1, 7)/7) - (pow(x-1, 8)/8) + (pow(x-1, 9)/9) - (pow(x-1, 10)/10) + (pow(x-1, 11)/11) - (pow(x-1, 12)/12) + (pow(x-1, 13)/13) - (pow(x-1, 14)/14))
+    if x < 2:
+        for i in range (1, 30):
+            ln = (((pow(-1, i+1))*(pow(x - 1, i)))/i)
+    else:
+        for i in range (1, 30):
+            ln = -(pow(-1, ( (i + 1 * (pow ( pow (x, -1) - 1, i) ) ) / i ) ) )
 
 valor = input("valor a ser calculado: ")
 valor_int = int (valor)
