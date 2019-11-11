ea89b63a3d9f44e025359d151f0fbec1fb821116
unknown <ra99829@uem.br>
2019-11-10 21:10:48 -0300

    sen cos arrumados, ln teste

--- modelo ieee 754/ln.py
+++ modelo ieee 754/ln.py
@@ -1,13 +1,18 @@
 import math
 from math import factorial
 from math import pow
-from ieee754 import *
 
 stop = 1E-11
 
+def calc_expoente(x):
+    return math.floor(math.log(x, 2))
+
+def plus_mantissa(x, e):
+    return pow(2, (math.log(x, 2) - e))
+
 def calculate_ln(x):
     k = calc_expoente(x)
-    x0 = plus_mantissa(x, k) - 1
+    x0 = plus_mantissa(x, k)
     g = math.log(x0, math.e)
     ln0_x = g + (k * math.log(2, math.e))
     return ln0_x
